57380d623a4504c0b4d02bd6a5b0012b0af70721
yxl <yxl@DESKTOP-SL7QJGE>
2017-04-07 00:10:02 +0800

    更新

--- scikit-learn/SVM.py
+++ scikit-learn/SVM.py
@@ -92,6 +92,11 @@ clf = svm.SVC(kernel='linear')
 clf.fit(X, Y)
 
 #get the separating hyperplane
+#仅在线性核中有  
+#====================================
+# w[0]*x0 + w[1]*x1 + b = 0 超平面方程
+# x1 = -(w[0] / w[1])*x0 - b / w[1]
+#====================================
 w = clf.coef_[0]
 a = -w[0] / w[1]
 xx = np.linspace(-5, 5)
@@ -115,5 +120,44 @@ plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, -1], s=80, facec
 plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=plt.cm.Paired)
 
 #'tight'使坐标轴的范围和数据保持一致。
+plt.axis('tight')
+plt.show()
+
+#------------------------Separating hyperplane for unblanced classes-------------------------------
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+
+#we create 40 separable points
+rng = np.random.RandomState(0)
+n_samples_1 = 1000
+n_samples_2 = 100
+X = np.r_[1.5 * rng.randn(n_samples_1, 2),
+          0.5 * rng.randn(n_samples_2, 2) + [2, 2]]
+y = [0] * (n_samples_1) + [1] * (n_samples_2)
+
+#fit the model and get the separating hyperplane
+clf = svm.SVC(kernel='linear', C=1.0)
+clf.fit(X, y)
+
+w = clf.coef_[0]
+a = -w[0] / w[1]
+xx = np.linspace(-5, 5)
+yy = a * xx - clf.intercept_[0] / w[1]
+
+#get the separating hyperplane using weighted classes
+#对类别为1的增加权重
+wclf = svm.SVC(kernel='linear', class_weight={1:10})
+wclf.fit(X, y)
+
+ww = wclf.coef_[0]
+wa = -ww[0] / ww[1]
+wyy = wa * xx - wclf.intercept_[0] / ww[1]
+
+#plot separating hyperplane and samples
+plt.plot(xx, yy, 'k-', label='no weights')
+plt.plot(xx, wyy, 'g--', label='with weights')
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired)
+plt.legend()
 plt.axis('tight')
 plt.show()
